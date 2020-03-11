3e61c128f53ba11d3eba160eb59b9e5a901abcf7
hanjun <hanjun>
2020-03-11 09:35:49 +0800

    init

--- src/main/java/com/example/springbootsharingjdbc/demo/controller/GoodsController.java
+++ src/main/java/com/example/springbootsharingjdbc/demo/controller/GoodsController.java
@@ -4,9 +4,12 @@ import com.example.springbootsharingjdbc.demo.mapper.GoodsMapper;
 import com.example.springbootsharingjdbc.demo.mapper.UsersMapper;
 import com.example.springbootsharingjdbc.demo.model.Goods;
 import com.example.springbootsharingjdbc.demo.model.Users;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import io.shardingsphere.core.keygen.KeyGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -89,5 +92,11 @@ public class GoodsController {
        Users user = new Users();
         user.setName("shangpin5");
         return usersMapper.selectByUserName(user);
+    }
+
+    @RequestMapping("selectByPage")
+    public PageInfo selectByPage(){
+        PageHelper.startPage(2,2);
+        return new PageInfo(goodsMapper.selectByParams());
     }
 }
